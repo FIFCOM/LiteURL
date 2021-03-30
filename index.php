954ecf4555bd1b827d518023976cf74d405ad050
FIFCOM <1280874899@qq.com>
2021-03-30 12:29:00 +0800

    Daily Update

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 require_once("config/config.php");
 require_once("pages/functions.php");
 
+echo lurlSet("https://fifcom.cn", "fcs", "114514", "0");
 ?>
--- pages/functions.php
+++ pages/functions.php
@@ -18,28 +18,39 @@ function lurlQRUri($string){
     return 'https://www.zhihu.com/qrcode?url='.urlencode($string);
 }
 
-function lurlSet($alias, $key){
+function lurlSet($uri, $alias, $key, $expire){
     $key = hash("ripemd128", $key);
+    if (lurlGet($alias, $key, 0) != 0) return 0;
+    $encryptedUri = base64_encode(openssl_encrypt($uri,'aes-128-cbc', $key, OPENSSL_RAW_DATA, LURL_CRYPT_IV));
+    $expire = $expire?$expire+time():"9999999999";
     $conn = mysqli_connect(LURL_DB_HOSTNAME, LURL_DB_USERNAME, LURL_DB_PASSWORD, LURL_DB_NAME);
     if (mysqli_connect_errno()) echo "Lite URL MySQL Connect Error : " . mysqli_connect_error();
-    return 0;
+    $sql = "INSERT INTO lurl (uri, alias, expire, count) VALUES ('$encryptedUri', '$alias', '$expire', 0)";
+    if ($conn->query($sql) === TRUE) {
+        return 1;
+    } else {
+        //echo "Lite URL MySQL Insert Error: " . $sql . "<br>" . $conn->error;
+        return 0;
+    }
+    /* bug */
 }
 
-function lurlGet($alias, $key){
+function lurlGet($alias, $key, $countpp){
     $key = hash("ripemd128", $key);
     $conn = mysqli_connect(LURL_DB_HOSTNAME, LURL_DB_USERNAME, LURL_DB_PASSWORD, LURL_DB_NAME);
     if (mysqli_connect_errno()) echo "Lite URL MySQL Connect Error : " . mysqli_connect_error();
     $result = mysqli_query($conn,"SELECT * FROM lurl WHERE alias='$alias'");
     $row = mysqli_fetch_array($result);
     $encryptedUri = $row['uri']; $expire = $row['expire'] - time(); $count = $row['count'];
-    $uri = openssl_decrypt($encryptedUri,'aes-128-cbc', $key, OPENSSL_RAW_DATA, LURL_CRYPT_IV);
+    mysqli_close($conn);
+    $uri = openssl_decrypt(base64_decode($encryptedUri),'aes-128-cbc', $key, OPENSSL_RAW_DATA, LURL_CRYPT_IV);
     if ($expire <= 0) lurlDelete($alias);
-    lurlCount($count + 1);
+    $countpp?lurlCount($count + 1):0;
     if (!$uri || $expire <= 0) return 0; else return $uri;
+    /* bug */
 }
 
 function lurlDelete($alias) {
-    $key = hash("ripemd128", $key);
     $conn = mysqli_connect(LURL_DB_HOSTNAME, LURL_DB_USERNAME, LURL_DB_PASSWORD, LURL_DB_NAME);
     if (mysqli_connect_errno()) echo "Lite URL MySQL Connect Error : " . mysqli_connect_error();
     return 0;
