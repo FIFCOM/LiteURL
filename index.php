120fdb04a7f1c620d7a6987ae2e14c63903636fe
FIFCOM <1280874899@qq.com>
2021-04-01 16:49:18 +0800

    Daily Update

--- index.php
+++ index.php
@@ -3,19 +3,24 @@ require_once("config/config.php");
 require_once("pages/functions.php");
 $SvrName = $_SERVER['HTTP_HOST'].str_replace('/index.php','',$_SERVER['PHP_SELF']);
 $lurlDefaltCustomAlias = lurlRandomToken(LURL_SHORTEST_ALIAS_LENGTH);
-//lurlSet("https://fifcom.cn/", "123", "123", 0);
-//lurlDelete("123");
+//lurlSet("https://bilibili.com/", "bili", "0", 0);
+lurlDelete("bili");
 
 if (isset($_REQUEST['alias'])) {
     if (isset($_REQUEST['key']))
     {
-        $redirectUri = lurlGet("$alias", "$key", "1");
-        header('Location: '.$redirectUri);
-    } else {
+        $redirectUri = lurlGet($_REQUEST['alias'], $_REQUEST['key'], "1");
+        if ($redirectUri) {
+            header('Location: '.$redirectUri);
+        } else {
+            header('Location: '.$lurlTLSEncryption.$SvrName.'/?xxx');
+        }
         
-        exit;
+    } else {
+        $redirectUri = lurlGet($_REQUEST['alias'], 0, "1");
+        header('Location: '.$redirectUri);
     }
 }
 
-//require_once("pages/lurlIndex.php");
+require_once("pages/lurlIndex.php");
 ?>
