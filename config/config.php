ee3826a012543a608b7ef68b0c86bdac71a1e227
FIFCOM <1280874899@qq.com>
2021-04-30 21:54:15 +0800

    Daily Update

--- config/config.php
+++ config/config.php
@@ -4,13 +4,11 @@ const PRIMARY_THEME = 'teal';
 const ACCENT_THEME = 'indigo';
 const SITE_NAME = 'LiteURL';
 const ICON_URL = 'https://q.qlogo.cn/headimg_dl?dst_uin=1280874899&spec=640';
-/*                */
 
 /* Advanced settings */
 const TLS_ENCRYPT = 'disable';
 const LURL_CRYPT_IV = '1234567890123456';
 const LITEURL_VERSION = '0.9.9b';
-/*                                   */
 
 /* DB settings */
 const LURL_DB_HOSTNAME = 'localhost';
@@ -19,7 +17,6 @@ const LURL_DB_PASSWORD = 'localhost';
 const LURL_DB_NAME = 'liteurl';
 const LURL_MIN_ALIAS_LENGTH = '6';
 const LURL_MAX_ALIAS_LENGTH = '16';
-/*             */
 
 error_reporting(E_ALL);
 $_ERROR = array();
